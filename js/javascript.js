a05d2077c772fc8a7d8fa998ba519b5da62ad562
tariqalsharif <you@example.com>
2018-02-16 20:25:17 +0200

    new design for custome page

--- js/javascript.js
+++ js/javascript.js
@@ -1,34 +1,34 @@
 // ======= Start Ready Function =======
-$(document).ready(function(){
+$(document).ready(function () {
 
 // ======= Language Function ==========
-  $("#en").on('click',function(){
-       $("html").attr("dir", "ltr");
-       $("html").attr("lang", "en");
-  });
-  $("#ar").on('click',function(){
-       $("html").attr("dir", "rtl");
-       $("html").attr("lang", "ar");
-  }); // End Language Function
-
-   //======= Carousael slider =======
-   $(document).ready(function() {
-    //Events that reset and restart the timer animation when the slides change
-    $("#transition-timer-carousel").on("slide.bs.carousel", function(event) {
-        //The animate class gets removed so that it jumps straight back to 0%
-        $(".transition-timer-carousel-progress-bar", this)
-            .removeClass("animate").css("width", "0%");
-    }).on("slid.bs.carousel", function(event) {
-        //The slide transition finished, so re-add the animate class so that
-        //the timer bar takes time to fill up
-        $(".transition-timer-carousel-progress-bar", this)
-            .addClass("animate").css("width", "100%");
+    $("#en").on('click', function () {
+        $("html").attr("dir", "ltr");
+        $("html").attr("lang", "en");
     });
+    $("#ar").on('click', function () {
+        $("html").attr("dir", "rtl");
+        $("html").attr("lang", "ar");
+    }); // End Language Function
 
-    //Kick off the initial slide animation when the document is ready
-    $(".transition-timer-carousel-progress-bar", "#transition-timer-carousel")
-        .css("width", "100%");
-});
+    //======= Carousael slider =======
+    $(document).ready(function () {
+        //Events that reset and restart the timer animation when the slides change
+        $("#transition-timer-carousel").on("slide.bs.carousel", function (event) {
+            //The animate class gets removed so that it jumps straight back to 0%
+            $(".transition-timer-carousel-progress-bar", this)
+                    .removeClass("animate").css("width", "0%");
+        }).on("slid.bs.carousel", function (event) {
+            //The slide transition finished, so re-add the animate class so that
+            //the timer bar takes time to fill up
+            $(".transition-timer-carousel-progress-bar", this)
+                    .addClass("animate").css("width", "100%");
+        });
+
+        //Kick off the initial slide animation when the document is ready
+        $(".transition-timer-carousel-progress-bar", "#transition-timer-carousel")
+                .css("width", "100%");
+    });
 
     $("#selectbtn").on('click', function () {
         if ($("html").attr("lang") === "en") {
@@ -38,22 +38,33 @@ $(document).ready(function(){
         }
     });
 
-    $(".removebtn").on('click', function(){
+    $(".removebtn").on('click', function () {
         var url = window.location.href;
-        $('.removebtn').parent().parent().css("display","none");
-        if($("html").attr("lang") === "en"){
+        $('.removebtn').parent().parent().css("display", "none");
+        if ($("html").attr("lang") === "en") {
             url = url.split('&')[0];
             history.pushState(null, null, url.replace(/&view=.*(&?)/, '$1'));
             $('.itemReview').html("<div class='col-md-12'><h4>The images have been removed, There are no photos selected! <a class='small' href='index.html'>back to home</a></h4></div>");
-            $('.summary').css('visibility','hidden');
+            $('.summary').css('visibility', 'hidden');
         } else {
             $('.itemReview').html("<div class='col-md-12'><h4>تم حذف المنتج، لا يوجد هناك صور مختارة! <a class='small' href='index.html'>العودة الى الرئيسية</a></h4></div>");
-            $('.summary').css('visibility','hidden');
+            $('.summary').css('visibility', 'hidden');
             url = url.split('&')[0];
             history.pushState(null, null, url.replace(/&view=.*(&?)/, '$1'));
         }
         event.stopPropagation();
     });
+
+    $(".productPhoto")
+            .hover(function () {
+                $(this).find("img").css('opacity', '0.3');
+                $(this).find(".caption").css('opacity', '0.3');
+                $(this).find("button").css('opacity', '1');
+            }, function () {
+                $(this).find("img").css('opacity', '1');
+                $(this).find(".caption").css('opacity', '1');
+                $(this).find("button").css('opacity', '0');
+            });
 
     console.log('Going Good');
 });    // ======= End Ready Function =======
@@ -61,6 +72,6 @@ $(document).ready(function(){
 
 
 // ======= Start Load Function =======
-$(window).load(function() {
+$(window).load(function () {
 
 });  // ======= End Load Function =======
--- uploadImages/js/script.js
+++ uploadImages/js/script.js
@@ -1,29 +1,30 @@
 // Get parameters from Url
 $.urlParam = function (name) {
-                var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
-                if (results == null) {
-                    return null;
-                } else {
-                    return results[1] || 0;
-                }
-            }
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    if (results == null) {
+        return null;
+    } else {
+        return results[1] || 0;
+    }
+}
 
-            // example.com?param1=name&param2=&id=6
-            $.urlParam('param1'); // name
+// example.com?param1=name&param2=&id=6
+$.urlParam('param1'); // name
 //            $.urlParam('id');        // 6
 //            $.urlParam('param2');   // null
-            var pnValue = decodeURIComponent($.urlParam('pn')).replace(/\+/g, ' ');
-            var inValue = decodeURIComponent($.urlParam('in')).replace(/\+/g, ' ');
-            var isValue = decodeURIComponent($.urlParam('is')).replace(/\+/g, ' ');
-            var qtyValue = decodeURIComponent($.urlParam('qty')).replace(/\+/g, ' ');
-            var totalValue = decodeURIComponent($.urlParam('total')).replace(/\+/g, ' ');
+var pnValue = decodeURIComponent($.urlParam('pn')).replace(/\+/g, ' ');
+var inValue = decodeURIComponent($.urlParam('in')).replace(/\+/g, ' ');
+var isValue = decodeURIComponent($.urlParam('is')).replace(/\+/g, ' ');
+var qtyValue = decodeURIComponent($.urlParam('qty')).replace(/\+/g, ' ');
+var totalValue = decodeURIComponent($.urlParam('total')).replace(/\+/g, ' ');
 
-            $("input[name='pn']").attr('value',pnValue);
-            $("input[name='in']").attr('value',inValue);
-            $("input[name='is']").attr('value',isValue);
-            $("input[name='qty']").attr('value',qtyValue);
-            $("input[name='total']").attr('value',totalValue);
-            
+$("input[name='pn']").attr('value', pnValue);
+$("input[name='in']").attr('value', inValue);
+$("input[name='is']").attr('value', isValue);
+$("input[name='qty']").attr('value', qtyValue);
+$("input[name='total']").attr('value', totalValue);
+
+
 // Initialize Firebase
 var config = {
     apiKey: "AIzaSyBasVDeO5TNFnFX-wB0MBr_Fh_vRCKYbOM",
@@ -39,58 +40,61 @@ var fileButton = document.getElementById('fileButton');
 
 //Listen for file selection
 $('#submit').on('click', function (e) {
-    if (pnValue == "10 Photos / 2 FREE"){
+    if (pnValue == "10 Photos / 2 FREE") {
         if (fileButton.files.length > 12 || fileButton.files.length < 10) {
             alert("Upload Only 10 Photos! Dont forget 2 photos free");
         } else {
             var imgIndex = 9;
             fileCheck(imgIndex);
         }
-        
-    } else if(pnValue == "20 Photos / 5 FREE"){
+
+    } else if (pnValue == "20 Photos / 5 FREE") {
         if (fileButton.files.length > 25 || fileButton.files.length < 20) {
             alert("Upload Only 20 Photos! Dont forget 5 photos free");
         } else {
             imgIndex = 19;
             fileCheck(imgIndex);
         }
-        
-    }else if(pnValue == "50 Photos / 20 FREE"){
+
+    } else if (pnValue == "50 Photos / 20 FREE") {
         if (fileButton.files.length > 70 || fileButton.files.length < 50) {
             alert("Upload Only 50 Photos! Dont forget 20 photos free");
         } else {
             imgIndex = 49;
-           fileCheck(imgIndex);
+            fileCheck(imgIndex);
         }
+    } else if (pnValue == "Custome Order") {
+        imgIndex = 50;
+        fileCheck(imgIndex);
     }
 });
 
-function fileCheck (imgIndex){
-     //check file type 
-        //this is from http://webdesigncolors.navayan.com/jquery-validation-for-file-type-extension/
-        var file = $("input[type='file']").val();
-        var exts = ['jpg', 'jpeg', 'png', 'gif'];
-        // first check if file field has any value
-        if (file) {
-            // split file name at dot
-            var get_ext = file.split('.');
-            // reverse name to check extension
-            get_ext = get_ext.reverse();
-            // check file type is valid as given in 'exts' array
-            if ($.inArray(get_ext[0].toLowerCase(), exts) > -1) {
-                //Get files
-                for (var i = 0; i < fileButton.files.length; i++) {
-                    var imageFile = fileButton.files[i];
-                    if (i >= imgIndex) {
-                        uploadImageAsPromise(imageFile);
-                        alert('Your Images have been submitted');
-                        $("#submit").attr('disabled','disa');
-                    }
+function fileCheck(imgIndex) {
+    //check file type 
+    //this is from http://webdesigncolors.navayan.com/jquery-validation-for-file-type-extension/
+    var file = $("input[type='file']").val();
+    var exts = ['jpg', 'jpeg', 'png', 'gif'];
+    // first check if file field has any value
+    if (file) {
+        // split file name at dot
+        var get_ext = file.split('.');
+        // reverse name to check extension
+        get_ext = get_ext.reverse();
+        // check file type is valid as given in 'exts' array
+        if ($.inArray(get_ext[0].toLowerCase(), exts) > -1) {
+            //Get files
+            for (var i = 0; i < fileButton.files.length; i++) {
+                var imageFile = fileButton.files[i];
+                if (i >= imgIndex) {
+                    uploadImageAsPromise(imageFile);
+                    alert('Your Images have been submitted');
+                    $("#submit").attr('disabled', 'disabled');
                 }
-            } else {
-                alert('Sory! You select invalid file OR there is a file are not image!');
             }
+        } else {
+            alert('Sory! You select invalid file OR there is a file is not image!');
         }
+    }
 };
 
 //Handle waiting to upload each file using promise
@@ -109,7 +113,7 @@ function uploadImageAsPromise(imageFile) {
                     $(".loading").show(200);
 //                    $('body').append("<progress value='" + percentage + "' max='100' id='uploader' style='-webkit-appearance: none;appearance: none;width: 50%;margin-bottom: 10px;'></progress><div style='color:#000;'>" + imageFile.name + imageFile.size + "</div>");
                     if ($('progress').attr('value') === '100') {
-                        $('#doneBtn').show("slow");   
+                        $('#doneBtn').show("slow");
                         $(".loading").hide(200);
                     }
                 },
@@ -117,7 +121,7 @@ function uploadImageAsPromise(imageFile) {
 
                 },
                 function complete() {
-                    var downloadURL = task.snapshot.downloadURL;                   
+                    var downloadURL = task.snapshot.downloadURL;
                 }
         );
     });
@@ -202,12 +206,12 @@ function uploadImageAsPromise(imageFile) {
                     .on('change', 'input', changeInput);
         }
     });
-    
+
 
 })(jQuery);
 
 $('[data-provide="fileupload"]').fileUpload();
- 
+
 $("#doneBtn").on('click', function () {
     if ($("html").attr("lang") === "en") {
         window.location.assign("../en-contact.html");
